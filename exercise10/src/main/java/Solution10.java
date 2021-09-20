7f1a278e599d5fc86a916ab9a384b14024ce6854
Adrian Chew <achewinquiries@gmail.com>
2021-09-19 21:56:42 -0400

    added ex10

---
+++ exercise10/src/main/java/Solution10.java
@@ -0,0 +1,69 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Adrian Chew
+ */
+
+    /*
+    forLoop counter=1; counter <=3; counter++
+        print "Enter the price of item 1:"
+        'in' = get input from user
+        'price' = parseInt('in')
+        print "Enter the quantity of item 1:"
+        'in' = get input from user
+        'count' = parseInt('in')
+        'subTotal' += price * count
+    'taxRate' = .055
+    'tax' = 'subtotal' * 'taxRate'
+    'total' = 'subTotal' + 'tax'
+    print "Subtotal: 'subTotal'\nTax: 'tax'\nTotal: 'total'"
+    */
+
+import java.util.Scanner;
+
+public class Solution10 {
+
+    private static final Scanner in = new Scanner(System.in);
+
+    public int getInputFromUser(String prompt) {
+        System.out.println(prompt);
+        return Integer.parseInt(in.nextLine());
+    }
+
+    public double calcSubTotal(double subTotal, int price, int count) {
+        subTotal += price * count;
+        return subTotal;
+    }
+
+    public double calcTax(double subTotal, double taxRate) {
+        return subTotal * taxRate;
+    }
+
+    public double calcTotal(double subTotal, double tax){
+        return subTotal + tax;
+    }
+
+    public void printTotal(double subTotal, double tax, double total) {
+        System.out.printf("Subtotal: %.2f%nTax: %.2f%nTotal: %.2f", subTotal, tax, total);
+    }
+
+    public static void main(String[] args) {
+        Solution10 app = new Solution10();
+
+        final double TAX_RATE = .055;
+
+        double subTotal = 0;
+        int price;
+        int count;
+
+        for(int counter = 1; counter <=3; counter++) {
+            price = app.getInputFromUser("Enter the price of item " + counter + ":");
+            count = app.getInputFromUser("Enter the quantity of item " + counter + ":");
+            subTotal = app.calcSubTotal(subTotal, price, count);
+        }
+
+        double tax = app.calcTax(subTotal, TAX_RATE);
+        double total = app.calcTotal(subTotal, tax);
+
+        app.printTotal(subTotal, tax, total);
+    }
+}
